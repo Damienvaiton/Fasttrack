9f96eb8d4f7361b77e1314fe8452a38c0007588e
Ewen-01 <ewen.crvs@gmail.com>
2023-11-15 16:11:14 +0100

    [FINISH] Finish the design card

--- app/src/main/java/com/iut/app/android/fasttrack/View/RankingAdapter.kt
+++ app/src/main/java/com/iut/app/android/fasttrack/View/RankingAdapter.kt
@@ -1,8 +1,10 @@
 package com.iut.app.android.fasttrack.View
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.iut.app.android.fasttrack.R
@@ -18,6 +20,11 @@ class RankingAdapter(private val ranking: DriverRanking) : RecyclerView.Adapter<
         val positionRanking = itemView.findViewById<TextView>(R.id.numeroDriver)
         val nameTeam = itemView.findViewById<TextView>(R.id.nameConstructorDriver)
         val points = itemView.findViewById<TextView>(R.id.nbPointPiloteRanking)
+        val background = itemView.findViewById<ImageView>(R.id.divRankingPrincipal2)
+        val constructorName = itemView.findViewById<TextView>(R.id.nameConstructorDriver)
+        val pointsDriver = itemView.findViewById<TextView>(R.id.nbPointPiloteRanking)
+        val photodriver = itemView.findViewById<ImageView>(R.id.imgdriver)
+        val phototeam = itemView.findViewById<ImageView>(R.id.imgteam)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RankingAdapter.ViewHolder {
@@ -36,21 +43,41 @@ class RankingAdapter(private val ranking: DriverRanking) : RecyclerView.Adapter<
 
         val PrenomDriver = ranking.MRData.StandingsTable.StandingsLists[0].DriverStandings[position].Driver.givenName.get(0)
         val NomDriver = ranking.MRData.StandingsTable.StandingsLists[0].DriverStandings[position].Driver.familyName
+        val IdDriver = ranking.MRData.StandingsTable.StandingsLists[0].DriverStandings[position].Driver.driverId
 
         val NomTeam = ranking.MRData.StandingsTable.StandingsLists[0].DriverStandings[position].Constructors[0].name
         val points = ranking.MRData.StandingsTable.StandingsLists[0].DriverStandings[position].points
+        val constructorId = ranking.MRData.StandingsTable.StandingsLists[0].DriverStandings[position].Constructors[0].constructorId
 
         // Set item views based on your views and data model
         val NomDriverTextView = viewHolder.nameDriver
+        val resources: Resources = viewHolder.itemView.context.resources
+        val resourceId: Int = resources.getIdentifier(constructorId, "color", viewHolder.itemView.context.packageName)
+        val resourceLogoConstructor: Int = resources.getIdentifier(constructorId, "drawable", viewHolder.itemView.context.packageName)
+        val resourcePhoto: Int = resources.getIdentifier(IdDriver, "drawable", viewHolder.itemView.context.packageName)
         val positionClassementTextView = viewHolder.positionRanking
         val NomTeamTextView = viewHolder.nameTeam
         val pointsTextView = viewHolder.points
+        val backgroundPilote = viewHolder.background
+        val ConstructorName = viewHolder.constructorName
+        val PointsDriver = viewHolder.pointsDriver
+        val PhotoDriver = viewHolder.photodriver
+        val PhotoTeam = viewHolder.phototeam
 
         //
         NomDriverTextView.setText(PrenomDriver + ". " + NomDriver)
         NomTeamTextView.setText(NomTeam)
         pointsTextView.setText(points+ " pts")
         positionClassementTextView.setText(position.plus(1).toString())
+
+        PhotoDriver.setImageDrawable(resources.getDrawable(resourcePhoto, null))
+        PhotoTeam.setImageDrawable(resources.getDrawable(resourceLogoConstructor, null))
+
+
+        backgroundPilote.setColorFilter(resources.getColor(resourceId, null))
+        ConstructorName.setTextColor(resources.getColor(resourceId, null))
+        PointsDriver.setTextColor(resources.getColor(resourceId, null))
+
     }
 
     // Returns the total count of items in the list
